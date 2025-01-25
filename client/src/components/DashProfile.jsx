ce8f7f5373d87643a65ab363de8cd1a851b12fbe
Suraiya Eva <aktersuraiya115@gmail.com>
2025-01-26 01:57:39 +0600

    complete user image upload functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,10 +1,15 @@
 import { Alert, Button, TextInput } from "flowbite-react";
 import React, { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
-import { getFirestore, collection, getDocs } from "firebase/firestore";
-import { app } from "../firebase";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
+import { app } from "../firebase";
 
 export default function DashProfile() {
   const { currentUser } = useSelector((state) => state.user);
@@ -30,120 +35,120 @@ export default function DashProfile() {
     }
   }, [imageFile]);
   const uploadImage = async () => {
-    {
-      /*service cloud.firestore {
-      match /databases/{database}/documents {
-        match /{document=**} {
-          allow read;
-          allow write:if
-          request.resource.size<2*1024*1024&&
-          request.resource.contentType.matches("image/.*")
+    /*service firebase.storage {
+        match /b/{bucket}/o {
+          match /{allPaths=**} {
+            allow read;
+            allow write:if
+            request.resource.size<2*1024*1024 &&
+            request.resource.contentType.matches("image/.*")
+          }
         }
-      }
-    }*/
-    }
+      }*/
+    setImageFileUploadError(null);
 
-    const storage = getFirestore(app);
+    const storage = getStorage(app);
     const fileName = new Date().getTime() + imageFile.name;
-    {
-      const storageRef = ref(storage, fileName);
-      const uploadTask = uploadBytesResumable(storageRef, imageFile);
-      uploadTask.on(
-        "state_changed",
-        (snapshot) => {
-          const progress =
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
 
-          setImageFileUploadProgress(progress.toFixed(0));
-        },
-        (error) => {
-          setImageFileUploadError(
-            "Could not upload image (File must be less than 2MB)"
-          );
-        },
-        () => {
-          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-            setImageFileUrl(downloadURL);
-          });
-        }
-      );
-    }
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, imageFile);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+
+        setImageFileUploadProgress(progress.toFixed(0));
+      },
+      (error) => {
+        setImageFileUploadError(
+          "Could not upload image (File must be less than 2MB)"
+        );
+        setImageFileUploadProgress(null);
+        setImageFile(null);
+        setImageFileUrl(null);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setImageFileUrl(downloadURL);
+        });
+      }
+    );
   };
 
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
-      <h1 className="my-7 text-center font-semibold text-2xl">
-        Profile
-        <form className="flex flex-col gap-4">
-          <input
-            type="file"
-            accept="image/*"
-            onChange={handleImageChange}
-            ref={filePickerRef}
-            hidden
-          />
-          <div
-            className="rel w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full"
-            onClick={() => filePickerRef.current.click()}
-          >
-            {imageFileUploadProgress && (
-              <CircularProgressbar
-                value={imageFileUploadProgress || 0}
-                text={`${imageFileUploadProgress}%`}
-                strokeWidth={5}
-                styles={{
-                  root: {
-                    width: "100%",
-                    height: "100%",
-                    position: "absolute",
-                    top: 0,
-                    left: 0,
-                  },
-                  path: {
-                    stroke: `rgba(62, 152, 199, ${
-                      imageFileUploadProgress / 100
-                    })`,
-                  },
-                }}
-              />
-            )}
-
-            <img
-              src={imageFileUrl || currentUser.profilePicture}
-              alt="user"
-              className={`rounded-full w-full h-full object-cover border-8 border-[lightgray] ${
-                imageFileUploadProgress &&
-                imageFileUploadProgress < 100 &&
-                "opacity-60"
-              }`}
+      <h1 className="my-7 text-center font-semibold text-2xl">Profile</h1>
+      Profile
+      <form className="flex flex-col gap-4">
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleImageChange}
+          ref={filePickerRef}
+          hidden
+        />
+        <div
+          className="relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full"
+          onClick={() => filePickerRef.current.click()}
+        >
+          {imageFileUploadProgress && (
+            <CircularProgressbar
+              value={imageFileUploadProgress || 0}
+              text={`${imageFileUploadProgress}%`}
+              strokeWidth={5}
+              styles={{
+                root: {
+                  width: "100%",
+                  height: "100%",
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                },
+                path: {
+                  stroke: `rgba(62, 152, 199, ${
+                    imageFileUploadProgress / 100
+                  })`,
+                },
+              }}
             />
-          </div>
-          {imageFileUploadError && (
-            <Alert color="failure">{imageFileUploadError}</Alert>
           )}
-          <TextInput
-            type="text"
-            id="username"
-            placeholder="username"
-            defaultValue={currentUser.username}
-          />
-          <TextInput
-            type="email"
-            id="email"
-            placeholder="email"
-            defaultValue={currentUser.email}
-          />
 
-          <TextInput type="password" id="password" placeholder="password" />
-          <Button type="submit" gradientDuoTone="purpleToBlue" outline>
-            Update
-          </Button>
-        </form>
-        <div className="text-red-400 flex justify-between mt-5">
-          <span className="cursor-pointer">Delete Account</span>
-          <span className="cursor-pointer">Sign Out</span>
+          <img
+            src={imageFileUrl || currentUser.profilePicture}
+            alt="user"
+            className={`rounded-full w-full h-full object-cover border-8 border-[lightgray] ${
+              imageFileUploadProgress &&
+              imageFileUploadProgress < 100 &&
+              "opacity-60"
+            }`}
+          />
         </div>
-      </h1>
+        {imageFileUploadError && (
+          <Alert color="failure">{imageFileUploadError}</Alert>
+        )}
+        <TextInput
+          type="text"
+          id="username"
+          placeholder="username"
+          defaultValue={currentUser.username}
+        />
+        <TextInput
+          type="email"
+          id="email"
+          placeholder="email"
+          defaultValue={currentUser.email}
+        />
+
+        <TextInput type="password" id="password" placeholder="password" />
+        <Button type="submit" gradientDuoTone="purpleToBlue" outline>
+          Update
+        </Button>
+      </form>
+      <div className="text-red-400 flex justify-between mt-5">
+        <span className="cursor-pointer">Delete Account</span>
+        <span className="cursor-pointer">Sign Out</span>
+      </div>
     </div>
   );
 }
--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,4 +1,5 @@
 // Import the functions you need from the SDKs you need
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -6,11 +7,11 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "mern-blog-40ff0.firebaseapp.com",
-  projectId: "mern-blog-40ff0",
-  storageBucket: "mern-blog-40ff0.firebasestorage.app",
-  messagingSenderId: "276090589879",
-  appId: "1:276090589879:web:cb436a2afe2f156936b332",
+  authDomain: "first-app-e77e7.firebaseapp.com",
+  projectId: "first-app-e77e7",
+  storageBucket: "first-app-e77e7.firebasestorage.app",
+  messagingSenderId: "407806797470",
+  appId: "1:407806797470:web:44e88a55d42f12d7b3bd8f",
 };
 
 // Initialize Firebase
